1b24df13118af2f9420a0bb5730545d6ffb693de
Yashashwini <juno.candy.21@gmail.com>
2021-04-24 22:36:48 +0530

    CodingChallenge3

---
+++ questionnaire/Q1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Q1.aspx.cs" Inherits="questionnaire.Q1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <p style="margin-left: 560px">
+                </p>
+        </div>
+        <p style="margin-left: 560px">
+            <asp:Label ID="Label1" runat="server" Text="1. Wht is the capital of India? "></asp:Label>          </p>
+        <p style="margin-left: 560px">
+            <asp:Label ID="Label2" runat="server" visible="false"></asp:Label>
+        </p>
+        <p style="margin-left: 560px">
+            <asp:Label ID="Label3" runat="server" visible="false"></asp:Label>
+        </p>
+        <p style="margin-left: 560px">
+            <asp:CheckBoxList ID="Options"   runat="server" OnSelectedIndexChanged="onlyone()" >
+                <asp:ListItem Text="Kolkata" Value ="1"></asp:ListItem>
+                <asp:ListItem Text ="Delhi" Value ="2"></asp:ListItem>
+                <asp:ListItem Text ="Guwahati" Value ="3" ></asp:ListItem>
+                <asp:ListItem Text ="Mumbai" Value ="4"></asp:ListItem>
+                <asp:ListItem  Text ="Patna" Value ="5"></asp:ListItem>
+            </asp:CheckBoxList>
+        </p>
+        <p style="margin-left: 560px">
+            &nbsp;</p>
+        <p style="margin-left: 560px">
+            <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+        &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="Next" OnClick="Button2_Click" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button3" runat="server" Text="Submit Test" OnClick="Button3_Click" />
+        </p>
+    </form>
+
+      <script type="text/javascript">
+          function onlyone() {
+
+
+              if (document.getElementById(Options).onchange) {
+                  alert("Inside JS");
+              }
+
+
+          }
+      </script>
+</body>
+  
+</html>
+
---
+++ questionnaire/Q1.aspx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace questionnaire
+{
+    
+    public partial class Q1 : System.Web.UI.Page
+    {
+        
+        
+        private static List<Answers> Answers = new List<Answers>();
+        private static int next_count;
+        private static int count_save;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {   if(!IsPostBack)
+            {
+                next_count = 1;
+                count_save = 1;
+                Button3.Enabled = false;
+            }
+           
+            ViewState["count"] = next_count;
+            ViewState["save"] = count_save;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            Answers ans = new Answers();
+
+            ans.questionno = (int)ViewState["save"];
+                
+
+            for(int i =0; i<Options.Items.Count;i++)
+            { 
+                if(Options.Items[i].Selected)
+                {
+                    ans.answer = Options.Items[i].Text;
+                }
+                
+            }
+            bool isCorrect = ValidateAnswers(ans);
+            if(!isCorrect)
+            {
+                ans.IsWrongAnswer = "This answer is Incorrect";
+            }
+            else
+            {
+                ans.IsWrongAnswer = string.Empty;
+            }
+            Answers.Add(ans);
+            count_save++;
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Options.ClearSelection();
+            if (ViewState["count"] != null)
+            {
+                next_count = (int)ViewState["count"] + 1;
+            }
+            
+               
+            switch (next_count)
+            {
+                case 2:
+                    Label1.Text = "2. What is the Capital of Assam?";
+                    break;
+                case 3:
+                    Label1.Text = "3. What is the Capital of West Bengal?";
+                    break;
+                case 4:
+                    Label1.Text = "4. What is the Capital of Maharashtra?";
+                    break;
+                case 5:
+                    Label1.Text = "5. What is the Capital of Bihar?";
+                    break;
+                
+                default:
+                    Label3.Text = "Please submit your Test";
+                    Label3.Visible = true;
+                    Label1.Visible = false;
+                    Button1.Visible = false;
+                    Button2.Visible = false;
+                    Button3.Enabled = true;
+                    Options.Visible = false;
+                    break;                    
+                        }
+
+                
+
+        }
+
+        public bool ValidateAnswers(Answers answers)
+        {
+            bool response = true;
+            if(answers.questionno == 1)
+            {
+                if(answers.answer != "Delhi")
+                {
+                    response = false;
+                }
+            }
+            if (answers.questionno == 2)
+            {
+                if (answers.answer != "Guwahati")
+                {
+                    response = false;
+                }
+            }
+            if (answers.questionno == 3)
+            {
+                if (answers.answer != "Kolkata")
+                {
+                    response = false;
+                }
+            }
+            if (answers.questionno == 4)
+            {
+                if (answers.answer != "Mumbai")
+                {
+                    response = false;
+                }
+            }
+            if (answers.questionno == 5)
+            {
+                if (answers.answer != "Patna")
+                {
+                    response = false;
+                }
+            }
+            return response;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Options.Visible = false;
+            Label1.Visible = false;
+            Button1.Visible = false;
+            Button2.Visible = false;
+            Button3.Visible = false;
+            Label3.Visible = false;
+            int count_wrong = 0;
+
+            foreach (var answer in Answers)
+            {
+                
+                if (answer.IsWrongAnswer != string.Empty)
+                {
+                    count_wrong++;
+                    //Label labelresult = new Label();
+                    
+                }
+               
+            }
+
+            if (count_wrong > 0)
+            {
+                Label2.Text = count_wrong + " Questions are incorrect";
+                Label2.Visible = true;
+            }
+        }
+
+        
+        
+    }
+    public class Answers
+    {
+       public int questionno { get; set; }
+       public string answer { get; set; }
+        public string IsWrongAnswer { get; set; }
+    }
+
+    
+}
